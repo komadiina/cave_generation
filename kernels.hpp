b9b042173c34d64c50cba352980b4dfccd43bc51
ognjen <79698642+komadiina@users.noreply.github.com>
2023-02-13 18:28:21 +0100

    Updated kernels.hpp

--- kernels.hpp
+++ kernels.hpp
@@ -13,9 +13,6 @@ std::vector<float> box_blur_9x9(81, 0.111f);
 
 // Invert kernels
 std::vector<int> invert_3x3 = {0, 0, 0, 0, -1, 0, 0, 0, 0};
-std::vector<int> invert_5x5 = {0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0};
-std::vector<int> invert_7x7 = {0,0,0,0,0,0,0, 0,0,0,-1,0,0,0, 0,0,0,0,0,0,0,};
-
 } // namespace Kernels
 
 // clang-format on
