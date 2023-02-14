1570ae808ea041b0155693a73f5c1a755d1dd82d
ognjen <79698642+komadiina@users.noreply.github.com>
2023-02-14 18:27:05 +0100

    Update kernels.hpp

--- kernels.hpp
+++ kernels.hpp
@@ -11,10 +11,8 @@ std::vector<float> box_blur_3x3(9, 0.111f);
 std::vector<float> box_blur_6x6(36, 0.111f);
 std::vector<float> box_blur_9x9(81, 0.111f);
 
-// Invert kernels
+// Invert kernel
 std::vector<int> invert_3x3 = {0, 0, 0, 0, -1, 0, 0, 0, 0};
-std::vector<int> invert_5x5 = {0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0};
-std::vector<int> invert_7x7 = {0,0,0,0,0,0,0, 0,0,0,-1,0,0,0, 0,0,0,0,0,0,0,};
 
 } // namespace Kernels
 
